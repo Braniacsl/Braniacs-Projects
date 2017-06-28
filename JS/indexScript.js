f57a337b9e84e173b159f4651259fd04b778c7b5
Braniacslambam <benjaminvicenteburt@gmail.com>
2017-06-28 14:04:23 +0100

    Fix2

--- JS/indexScript.js
+++ JS/indexScript.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
 $('#Balls_Image').hover(function () {
 		$(this).html("<a href='https://github.com/Braniacslambam/OpenTKSharp-Bouncing-Balls/tree/master'><img src='./Images/Complete.png' class='Inner_Image' style='height:70%; width:70%' ></img></a><figcaption>Bouncing Balls C# Project Using OpenTK.</figcaption>");
-		$('#Pong_Image').html('<img src="./Images/Pong.png" class="Inner_Image"></img><figcaption>Pong Game Using Pygame</figcaption>');
+		$('#Pong_Image').html('<img src="./Images/Pong.PNG" class="Inner_Image"></img><figcaption>Pong Game Using Pygame</figcaption>');
 }, function() {
 	$(this).html('<img src="./Images/Balls.png" class="Inner_Image"></img><figcaption>Bouncing Balls C# Project Using OpenTK.</figcaption>');
 });
@@ -9,6 +9,6 @@ $('#Pong_Image').hover(function () {
 		$(this).html("<a href='https://github.com/Braniacslambam/Pong-Game-Using-Pygame'><img src='./Images/Complete1.png' class='Inner_Image' style='height:70%; width:70%' ></img></a><figcaption>Pong Game Using Pygame</figcaption>");
 		$('#Balls_Image').html('<img src="./Images/Balls.png" class="Inner_Image"></img><figcaption>Bouncing Balls C# Project Using OpenTK.</figcaption>');
 }, function() {
-	$(this).html('<img src="./Images/Pong.png" class="Inner_Image"></img><figcaption>Pong Game Using Pygame</figcaption>');
+	$(this).html('<img src="./Images/Pong.PNG" class="Inner_Image"></img><figcaption>Pong Game Using Pygame</figcaption>');
 });
 });
